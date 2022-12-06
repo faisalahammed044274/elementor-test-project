4eddddeb8f78b626ddeeb45b880c5dbee1f1f4f1
faisalahammed044274 <setu2014@gmail.com>
2022-12-07 02:39:23 +0600

    Simple Header

--- wp-content/themes/testheme/header.php
+++ wp-content/themes/testheme/header.php
@@ -11,49 +11,60 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes();?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset');?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+	<?php wp_head();?>
 </head>
-
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+			<body <?php body_class();?>>
+			<?php wp_body_open();?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'testheme' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'testheme');?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+		<div class="container ">
+			<div class="row align-items-center">
+			<div class="col-xl-3">
+				<!-- .site-branding -->
+			<div class="site-branding">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$testheme_description = get_bloginfo( 'description', 'display' );
-			if ( $testheme_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $testheme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
-
+the_custom_logo();
+if (is_front_page() && is_home()):
+?>
+<h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name');?></a></h1>
+<?php
+else:
+?>
+<p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name');?></a></p>
+<?php
+endif;
+$testheme_description = get_bloginfo('description', 'display');
+if ($testheme_description || is_customize_preview()):
+?>
+<p class="site-description"><?php echo $testheme_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped                 ?></p>
+	<?php endif;?>
+		</div>
+		<!-- .site-branding -->
+			</div>
+			<div class="col-xl-9">
+				<!-- #site-navigation -->
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'testheme' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'testheme');?></button>
 			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
+wp_nav_menu(
+    array(
+        'theme_location' => 'menu-1',
+        'menu_id' => 'primary-menu',
+    )
+);
+?>
 		</nav><!-- #site-navigation -->
+			</div>
+</div>
+		</div>
+
+
 	</header><!-- #masthead -->
